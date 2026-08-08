9d34f80e89ce48ff594615e970a9b1c067de7b3b
nadarselveshcs242534-creator <nadarselveshcs242534@gmail.com>
2026-08-08 11:28:15 +0530

    Header.jsx

--- hackathon-js/src/components/Header.jsx
+++ hackathon-js/src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className="bg-blue-600 text-white py-3 px-6">
       <div className="max-w-7xl mx-auto flex justify-center items-center">
         <img 
-          src="https://res.cloudinary.com/dniaxogth/image/upload/v1756024783/62-LOGO-HEADER-GOLD_oh3tzz.png" 
+          src="https://res.cloudinary.com/bfw5etsj/image/upload/v1786168628/MVLU_LOGO_63_pb5csn.jpg" 
           alt="College Logo" 
           className="h-12"
         />
